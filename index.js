60824d2ca10509c2c13fcff3e71baffccc884b99
noosrat-jahan <noosratmeem@gmail.com>
2025-04-20 11:22:59 +0600

    get and post api is created to post user data and view them in the the alluser page

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-require('dotenv').config()
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const app = express()
-const port = process.env.PORT || 5000 
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(express.json())
-
+app.use(express.json());
+app.use(cors())
 
 //Auth_CRUD
 //ccSuYvteTfpunfwl
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nx8zq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,7 +20,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -31,11 +28,27 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db("crud_app_users").collection("users");
 
-    
+
+    app.get("/users", async(req, res) =>{
+      const cursor = userCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.post("/users", async(req, res) =>{
+        const user = req.body 
+        const result = await userCollection.insertOne(user)
+        res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -43,11 +56,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Mern Role CRUD application is running.....");
+});
 
-app.get('/', (req, res )=>{
-    res.send("Mern Role CRUD application is running.....")
-})
-
-app.listen(port, ()=>{
-    console.log(`Mern Role CRUD application is running at port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Mern Role CRUD application is running at port ${port}`);
+});

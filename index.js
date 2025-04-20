35d2057ed70e930c04ccf0c17fa61e74318e0fed
noosrat-jahan <noosratmeem@gmail.com>
2025-04-20 22:39:15 +0600

    all relevant api for get, post and put is created for managing user info

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+
+var jwt = require('jsonwebtoken');
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 //Auth_CRUD
 //ccSuYvteTfpunfwl
@@ -30,19 +33,58 @@ async function run() {
 
     const userCollection = client.db("crud_app_users").collection("users");
 
+    // jwt related api
 
-    app.get("/users", async(req, res) =>{
-      const cursor = userCollection.find()
-      const result = await cursor.toArray()
+    app.post("/jwt", async(req, res) =>{
+      const user = req.body 
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '2m'})
+      res.send({token})
+    })
+
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/users/role/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
+    
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+
+    app.put('/users/role/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const updatedData = req.body
+      const updateDoc = {
+        $set: {
+          name: updatedData.name,
+          email: updatedData.email,
+          role: updatedData.role,          
+        }
+      }
+      const result = await userCollection.updateOne(filter, updateDoc, options)
       res.send(result)
     })
-
-    app.post("/users", async(req, res) =>{
-        const user = req.body 
-        const result = await userCollection.insertOne(user)
-        res.send(result)
-    })
-
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
